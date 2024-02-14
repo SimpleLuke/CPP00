251462c25ef018b1401d219cad6ba3450e71210b
Luke Lai <llai@c3r3s1.42london.com>
2024-02-14 15:31:34 +0000

    Add displayAccountsInfos

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: llai <llai@student.42london.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 14:42:06 by llai              #+#    #+#             */
-/*   Updated: 2024/02/14 15:18:42 by llai             ###   ########.fr       */
+/*   Updated: 2024/02/14 15:31:00 by llai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ Account::Account (int initial_deposit)
 	this->_amount = initial_deposit;
 	this->_nbDeposits = 0;
 	this->_nbWithdrawals = 0;
+	Account::_totalAmount += initial_deposit;
 	Account::_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";" << "amount:" << this->_amount << ";" << "created" << std::endl;
 	Account::_nbAccounts++;
@@ -39,4 +40,33 @@ void	Account::_displayTimestamp(void)
 	std::cout << "[" << lt->tm_year + 1900 << std::setfill('0') << std::setw(2) << lt->tm_mon + 1 << std::setfill('0') << std::setw(2) << lt->tm_mday << "_" << std::setfill('0') << std::setw(2) << lt->tm_hour << std::setfill('0') << std::setw(2) << lt->tm_min << std::setfill('0') << std::setw(2) << lt->tm_sec << "] ";
 }
 
+int		Account::getNbAccounts(void)
+{
+	return Account::_nbAccounts;
+}
+
+int		Account::getTotalAmount(void)
+{
+	return Account::_totalAmount;
+}
+
+int		Account::getNbDeposits(void)
+{
+	return Account::_totalNbDeposits;
+}
+
+int		Account::getNbWithdrawals(void)
+{
+	return Account::_totalNbWithdrawals;
+}
+
+void	Account::displayAccountsInfos(void)
+{
+	Account::_displayTimestamp();
+	std::cout << "accounts:" << Account::_nbAccounts << ";total:"<< Account::_totalAmount << ";deposits:" << Account::_totalNbDeposits << ";withdrawals:" << Account::_totalNbWithdrawals << std::endl;
+}
+
 int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
